48d32a0ce2570f360157428dddd4d4d1d7906773
Roman Provazník <dzoukr@dzoukr.cz>
2021-07-19 21:06:18 +0200

    Bio updates

--- src/MindfulYoga.Client/AboutMe/View.fs
+++ src/MindfulYoga.Client/AboutMe/View.fs
@@ -1,6 +1,7 @@
 module MindfulYoga.Client.AboutMe.View
 
 open Fulma
+open Feliz
 open Fable.React
 open Fable.React.Props
 open MindfulYoga.Client
@@ -14,95 +15,21 @@ let view =
         ]
         textSection [
             h1 [] [ str "Ing. Jana Provazníková"]
-            p [] [
-                str "Poprvé jsem se setkala s jógou na začátku mého studia na vysoké škole, kdy jsem
-                    sezením nad knížkami trávila dlouhé hodiny. Přišly bolesti zad, ztuhlá krční páteř
-                    a od té velmi časté bolesti hlavy spojené také s notnou dávkou stresu. V té době mi
-                    jóga s mými zdravotními problémy hodně pomáhala."
-            ]
-            p [] [
-                str "Po vystudování Vysoké školy ekonomické v Praze jsem se po hlavě vrhla do
-                    zaměstnání s vidinou slibné kariéry. Zanedlouho jsem pocítila, že tudy zřejmě má
-                    cesta nepovede. Uspokojené ego si sice libovalo na pozici manažera, ale
-                    nedokázalo vykompenzovat notnou dávku stresu a spěchu v každodenním životě."
-            ]
-            p [] [
-                str "Narodily se mi dvě dcery a já měla dostatek času si promýšlet, co dál. A jelikož mě
-                    jóga bavila a chtěla jsem ji studovat více do hloubky, rozhodla jsem se pro lektorský
-                    kurz. Jak jinak se člověk nejvíce o něčem dozví a naučí, než že se rozhodne to učit."
-            ]
-            p [] [
-                str "Začala moje cesta objevování a poznávání sama sebe, zjišťování, kdo vlastně jsem a
-                    čemu se chci v životě věnovat."
-            ]
-            p [] [
-                str "V roce 2016 jsem začala jógu učit. Byl to takový startovací můstek pro různá další
-                    školení a hledání, co jóga znamená pro mě samou. Meditace v té době byla pro mě
-                    něco naprosto nedosažitelného, nicméně něco mě na ní pořád lákalo. Přečetla jsem
-                    hodně knih, ale sednout si a meditovat, se mi nedařilo. Řekla jsem si, že pro začátek
-                    by mi mohl pomoci nějaký kurz, skupina podobně smýšlejících lidí se zájmem o
-                    stejnou věc."
-            ]
-            p [] [
-                str "V roce 2017 jsem prošla prvním meditačním kurzem. Zlomový byl pro mne ale až
-                    opět lektorský kurz RMT (Relation Mindfulness Training), kdy jsem se začala zabývat
-                    Mindfulness neboli meditací všímavosti. Během každodenních meditací jsem si
-                    uvědomila, jak cenný nástroj poznání meditace je."
-            ]
-            p [] [
-                str "Několik měsíců mi vrtalo hlavou, jak meditaci začlenit do lekcí jógy, jak jógovou praxi
-                    obohatit, aby přinášela co největší užitek. V hlavě mi zrála myšlenka o propojení
-                    meditace (mindfulness) s tělem a pohybem, až se jednoho krásného letního dne
-                    zrodil nápad "
-                a [ Class "mindful-link"; Href Router.MindfulYoga.Path; OnClick Router.goToUrl ] [ str "Mindful yogy®"]
-                str "."
-            ]
-            p [] [
-                str "Jóga je především o seberozvoji. Učí nás mít se rád, přijímat se a otevřít se… sobě i
-                    celému světu."
-            ]
-            p [] [
-                str "Cílem je nacházet vlastní cestu, žít svůj život a prožívat ho s lehkostí a klidnou myslí.
-                    S radostí pozoruji pokroky svých studentů, a i když výchozí pozici nemáme všichni
-                    stejnou, každý se může posouvat kupředu. Vše přijde v ten správný čas."
-            ]
-            p [] [
-                str "Učme se vděčnosti, laskavosti a lásce k sobě i ke svému okolí."
-            ]
-            p [] [
-                str "Ze srdce děkuji všem svým učitelům, minulým, současným i budoucím. Děkuji své
-                    rodině, mým dětem a přátelům … a Vám všem, svým klientům, i díky Vaší zpětné
-                    vazbě vím, že jsem na té správné cestě, a to mě posouvá stále dál."
-            ]
-            p [] [
-                str "Žít všímavě, znamená vést bohatý život."
-            ]
-            p [] [
-                str "Namasté"
-                br []
-                str "Jana"
-            ]
-        ]
-        
-        emptySection
-        textSection [
-            h1 [] [ str "Vzdělání"]
-            [
-                "2020", "Lektorka na projektu Terapeutická technika jako pomoc při vyrovnávání se s duševními obtížemi, Toseví z.s."
-                "2020-24", "Pražská vysoká škola psychosociálních studií, Daseinsanalytický psychoterapeutický výcvik"
-                "2019", "Meditační ústraní, Jeff Oliver (AUS)"
-                "2019", "Úvod do zaklánění, Dalibor Štedronský"
-                "2018/2019", "Roční výcvik pro lektory programu Relational Mindfulness Training (RMT), Jan Burian, Marek Vich"
-                "2018", "Stoje na rukách, Klára Pokorná"
-                "2018", "Umění jógového dotyku (techniky adjustementu), Ladislav Pokorný, Ánanda Ashtanga Yoga Shala"
-                "2018", "Hlas dokořán – vědomá práce s hlasem, Monika Obermajerová"
-                "2018", "Zdravá jóga aneb nejčastější chyby při nastavování jógových pozic, Mgr. Daniel Müller, IQ pohyb Akademie"
-                "2017", "Yin Yoga training, Certificate of Training Yin Yoga Teacher, 50 HR Yoga Alliance, Angela Jervis-Read, Yogacentrum in Prague"
-                "2017", "Zdravá ramena, Lucie Konigová"
-                "2017", "Kurz čakra jóga, Barbora Hu"
-                "2017", "Jóga pro seniory, FISAF"
-                "2016", "Hatha yoga training, Certificate of Training Hatha Yoga Teacher, 200 HR of in-class instruction and 100 HR of independent study, RYT Yoga Alliance Yogacentrum in Prague"
-                "2012", "Reiki 1. stupeň podle systému Dr. Mikao Usui, Martina Scales, Centrum Effeta"
-            ] |> in2ColTable
+            Html.p "Ve svém dospívání jsem se viděla jako úspěšná manažerka v kostýmku. Vystudovala jsem ekonomii a vrhla se do zaměstnání. Říká se, dej si pozor, co si přeješ, mohlo by se ti to vyplnit."
+            Html.p "Místo té usměvavé a spokojené manažerky, kterou jsem si vysnila, jsem se cítila vyčerpaná a naštvaná. Samotná práce mě příliš nenaplňovala a vystresovaní nadřízení pro mě byli spíše odstrašujícím příkladem." 
+            Html.p "Po mateřské jsem se znovu rozhodovala, co chci v životě dělat, hledala jsem svoji vášeň, své poslání. Jóga byla už od dvaceti let, kdy jsem ji v Praze při studiu na vysoké škole objevila, mým velkým koníčkem a já vždy chtěla pomáhat lidem, tak se v té době zrodil úplně bláznivý nápad, stát se lektorkou jógy." 
+            Html.p "Vše začalo v roce 2016, kdy jsem se prošla 200HR učitelským kurzem registrovaným pod Yoga Allliance a stala certifikovanou lektorkou jógy.  Od té doby provádím klienty na jejich jógové cestě, pravidelně praktikuji a dál se vzdělávám." 
+            Html.p "Skrze jógovou praxi jsem si vyřešila bolesti zad a zbavila se migrén. Zajímala jsem se o principy zdravého pohybu, ale i o přesah fyzické jógové praxe. Když jsem tehdy hovořila o meditacích s mou tehdejší lektorkou, jen se usmála a řekla, že na nějakou meditaci s dvěma malýma dětma můžu zapomenout. Teď už vím, že se mýlila, že meditovat může opravdu každý, i když má pocit, že nemá ani vteřinu volného času." 
+            Html.p "V roce 2017 jsem se připojila do meditující skupiny v budhistickém centru Lotos, kde jsem se poprvé dozvěděla o mindfulness (česky „všímavosti“), jejímž hlavním cílem je rozvíjení schopnosti vnímat a uvědomovat si události v přítomném okamžiku." 
+            Html.p "Postupně jsem si daleko více začala uvědomovat, kdo jsem, co cítím a co potřebuji." 
+            Html.p "„Všímavost“ začala vstupovat do mé jógové praxe a já se stále častěji přistihovala ve chvílích, kdy nevnímám – kdy jedu na autopilota, kdy jsem odpojená od svého těla, kdy překračuji svoje hranice. Všímala jsem si chvil, kdy jsem byla vtažena do kolotoče svých myšlenek a jako naprogramovaný stroj prováděla pozice. Častokrát moje pozornost směřovala spíše ven než dovnitř. Objevovaly se pocity méněcennosti, které vznikaly po porovnávání se s ostatními. A co víc toho, čeho jsem si všímala na jógové podložce, jsem viděla objevovat se i v mém každodenním životě. A nebyla to vůbec příjemná uvědomění, ale díky nim jsem mohla začít svou jógovou praxi a i život opravdu měnit." 
+            Html.p "Zároveň jsem si uvědomovala obrovskou hodnotu praxe, při které se mi dařilo se propojovat se svým tělem a vnímat přítomný okamžik, bylo to jako bych objevila poklad, který jsem si přála sdílet."
+            Html.p "Jistou úroveň „všímavosti“ má každý z nás, důležité je, že všímavost se dá pomocí jednoduchých technik rozvíjet a prohlubovat." 
+            Html.p "Abych si tuto dovednost a znalosti ještě více prohloubila absolvovala jsem roční výcvik a stala se certifikovanou lektorkou Relation Mindfulness, které se zabývá rozvíjením všímavosti nejenom ve vztahu k sobě, ale i ve vztahu k ostatním. Dále jsem čerpala informace a zkušenosti skrze další výcviky, kurzy a především vlastní jógovo-meditační praxi.  Velmi významnou událostí pro mě bylo meditační ústraní, kde jsem měla svůj první vhled a zážitek plného vnímání přítomného okamžiku." 
+            Html.p "Tak jak se u mě „všímavost“ rozvíjela, má jógová praxe přecházela do meditace v pohybu, poznatky z jógové podložky začaly víc a víc prostupovat do mého každodenního života a všechno se začalo najednou propojovat."
+            Html.p "V roce 2018 jsem založila Mindful yogu® – Vědomou jógu přítomného okamžiku se záměrem pomáhat lidem vědomě prožívat jógovou praxi, přibližovat se přítomnému okamžiku a dostávat se z bytí v hlavě zpátky do těla." 
+            Html.p "Během praxe vám pomohu naučit se důvěřovat svému tělu, číst a naslouchat jeho signálům, tak abyste bezpečně poznávali svoje hranice. Povedu vás ven z kolotoče vašich myšlenek zpět do těla, k dechu a pohybu. Naučíte se regulovat své emoce a vytvářet si pauzy pro svobodné rozhodnutí, jak zareagovat." 
+            Html.p "Vytvářím prostředí, ve kterém snadno zpomalíte a skrze rozvíjení všímavosti a vracení se k tomu, co se děje tady a teď, se posunete na další stupeň vaší praxe. To vše pak volně začne přecházet do vašeho každodenního života, mimo jógovou podložku a obohacovat ho o větší míru kontroly pozornosti a sebe-uvědomění."  
+            Html.p "Provádím jógovou praxí, která vás povede cestou poznávání sama sebe, k prožívání života ve všech jeho barvách - vědomě TADY a TEĎ." 
         ]
     ]
